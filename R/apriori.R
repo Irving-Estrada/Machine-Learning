52e82e66c4e1510040c08a8f43cde398ef7d0474
Irving-Estrada <63019475+Irving-Estrada@users.noreply.github.com>
2020-06-04 14:21:37 -0500

    Add files via upload

---
+++ R/apriori.R
@@ -0,0 +1,20 @@
+#Apriori 
+
+#Cargar dataset
+#install.packages('arules')
+library(arules)
+dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
+dataset = read.transactions('Market_Basket_Optimisation.csv',
+                           sep = ",", rm.duplicates = TRUE)
+
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 10)
+
+#Apriori
+rules = apriori(data = dataset,parameter = list(support = 0.004, confidence = 0.2))
+
+#Visualizacion de Resultados
+inspect(sort(rules,by = 'lift')[1:10])
+
+
+
